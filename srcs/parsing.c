18074f42c942691037c35b413280eee570bdfbf4
maxime <maxime.cauchy93@gmail.com>
2025-02-17 11:03:03 +0100

    push auto

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: mcauchy <mcauchy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 11:12:12 by mcauchy           #+#    #+#             */
-/*   Updated: 2025/02/17 11:02:27 by mcauchy          ###   ########.fr       */
+/*   Updated: 2025/02/17 11:03:02 by mcauchy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	single_parser(char *line)
 	char	**tab;
 
 	tab = ft_split(line, ' ');
-	// if (!tab)
-	// 	return (ft_putendl_fd("Error", 2), exit(1));
 	check_input(tab);
 	insert_list(tab);
 	print_list();
